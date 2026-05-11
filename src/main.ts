2090b5013fd99a7e7f18fb8af1bcb3e4e61aa6e4
anhnhatnhumko <anhnhathuynhvo@gmail.com>
2026-05-12 02:21:58 +0700

    fix railway main

--- src/main.ts
+++ src/main.ts
@@ -3,37 +3,37 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
 
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule, { rawBody: true });
-//   app.use(cookieParser());
-
-//   app.useGlobalPipes(
-//     new ValidationPipe({
-//       whitelist: true,
-//       forbidNonWhitelisted: true,
-//       transform: true,
-//     }),
-//   );
-
-//   app.enableCors({
-//     origin: process.env.FRONTEND_URL || 'http://localhost:3000',
-//     credentials: true,
-//   });
-
-//   await app.listen(process.env.PORT ?? 9999, '0.0.0.0');
-// }
-// bootstrap();
-
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { rawBody: true });
+  app.use(cookieParser());
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
 
   app.enableCors({
-    origin: true,
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
     credentials: true,
   });
 
-  const port = process.env.PORT || 9999;
-
-  await app.listen(port, '0.0.0.0');
+  await app.listen(process.env.PORT ?? 9999, '0.0.0.0');
 }
 bootstrap();
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+
+//   app.enableCors({
+//     origin: true,
+//     credentials: true,
+//   });
+
+//   const port = process.env.PORT || 9999;
+
+//   await app.listen(port, '0.0.0.0');
+// }
+// bootstrap();
